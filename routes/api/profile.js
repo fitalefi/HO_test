f9500d9d99b86f885feb5a440095aad596f2a812
efi fital <fitalefi@gmail.com>
2019-11-08 12:42:47 +0200

    get profile by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -68,4 +68,26 @@ router.post('/', [
   }
 ]);
 
+router.get('/', auth, async (req, res) => {
+  try {
+    const profiles = await Profile.find();
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.params.user_id });
+    if (!profile)
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
